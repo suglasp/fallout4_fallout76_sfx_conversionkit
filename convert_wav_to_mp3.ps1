1ab6e13eb72746f481a202c9c078b3fad9c8d8ba
Suglasp <suglasp@users.noreply.github.com>
2023-01-15 18:24:23 +0100

    Improved script readability
    Improved script readability

--- convert_wav_to_mp3.ps1
+++ convert_wav_to_mp3.ps1
@@ -2,41 +2,42 @@
 #
 # Pieter De Ridder
 # Script to convert wav (RIFF) to mp3 (Compressed) in a loop
+# https://github.com/suglasp/pwsh_convert_wav_to_mp3
 # created : 25/02/2020
-# updated : 11/01/2021
+# updated : 15/01/2023
 #
 # Usage:
 # .\convert_wav_to_mp3.ps1 [-CustomDir <custom_path_directory>]
 #
 
 # Global vars
-$global:WorkingDir = $($PSScriptRoot)
-$global:WorkingDirFFMPEG = "$($global:WorkingDir)\ffmpeg\bin"
-$global:ffmpeg = "$($global:WorkingDirFFMPEG)\ffmpeg.exe"
+$Global:WorkingDir = "$($PSScriptRoot)"
+$Global:WorkingDirFFMPEG = "$($Global:WorkingDir)\ffmpeg\bin"
+$Global:ffmpeg = "$($Global:WorkingDirFFMPEG)\ffmpeg.exe"
 
 
 #
-# Function : Convert-ToMP3
+# Function : Convert-ToWav2MP3
 # Convert wav file to mp3 with ffmpeg.exe 
 #
-Function Convert-ToMP3 {
+Function Convert-ToWav2MP3 {
 
     Param(
         [string]$WAVFile
     )
 
-    If (Test-Path -Path $global:ffmpeg) {
+    If (Test-Path -Path $Global:ffmpeg) {
         If (Test-Path -Path $WAVFile) {
             If ($WAVFile.EndsWith(".wav")) {
-                $sOutputPath = Split-Path $WAVFile -Parent
+                [String]$sOutputPath = Split-Path $WAVFile -Parent
 
-                $sOutputFile = (Split-Path $WAVFile -Leaf)
-                $sOutputFile = $sOutputFile.Substring(0, ($sOutputFile.Length -3)) + "mp3"
+                [String]$sOutputMp3File = (Split-Path $WAVFile -Leaf)
+                $sOutputMp3File = $sOutputMp3File.Substring(0, ($sOutputMp3File.Length -3)) + "mp3"
 
-                $sOutput = "$($sOutputPath)\$($sOutputFile)"
+                [String]$sOutputMp3 = "$($sOutputPath)\$($sOutputMp3File)"
 
-                If (-not (Test-Path $sOutput)) {
-                    $sProcArgs = "-i $([char]34)$($WAVFile)$([char]34) -vn -ar 44100 -ac 2 -b:4 192k $([char]34)$($sOutput)$([char]34)" 
+                If (-not (Test-Path -Path $sOutputMp3)) {
+                    $sProcArgs = "-i $([char]34)$($WAVFile)$([char]34) -vn -ar 44100 -ac 2 -b:4 192k $([char]34)$($sOutputMp3)$([char]34)" 
                     #$arrProcArgs = @()
                     #$arrProcArgs += "-i" 
                     #$arrProcArgs += "$([char]34)$($WAVFile)$([char]34)"
@@ -47,14 +48,14 @@ Function Convert-ToMP3 {
                     #$arrProcArgs += "2"
                     #$arrProcArgs += "-b:a"
                     #$arrProcArgs += "192k"
-                    #$arrProcArgs += "$([char]34)$($sOutput)$([char]34)"
+                    #$arrProcArgs += "$([char]34)$($sOutputMp3)$([char]34)"
 
-                    Write-Host "Generating $($sOutputFile)..."
-                    #$p = Start-Process -FilePath $global:ffmpeg -WorkingDirectory $global:WorkingDirFFMPEG -ArgumentList $arrProcArgs -NoNewWindow -Wait -PassThru
+                    Write-Host "Generating $($sOutputMp3File)..."
+                    #$p = Start-Process -FilePath $Global:ffmpeg -WorkingDirectory $Global:WorkingDirFFMPEG -ArgumentList $arrProcArgs -NoNewWindow -Wait -PassThru
                     
                     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
-                    $pinfo.FileName = $global:ffmpeg
-                    $pinfo.WorkingDirectory = $global:WorkingDirFFMPEG
+                    $pinfo.FileName = $Global:ffmpeg
+                    $pinfo.WorkingDirectory = $Global:WorkingDirFFMPEG
                     $pinfo.Arguments = $sProcArgs
                     $pinfo.CreateNoWindow = $true
                     $pinfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
@@ -69,18 +70,18 @@ Function Convert-ToMP3 {
                     $p.WaitForExit()
 
                     if ($p.ExitCode -eq 0) {
-                        Write-Warning "ffmpeg : success"
-                    } else {
-                        Write-Warning "ffmpeg : failed? [Exitcode $($p.ExitCode)]"
+                        Write-Warning "mp3 conversion : success"
+                    } Else {
+                        Write-Warning "mp3 conversion : failed? [Exitcode $($p.ExitCode)]"
                     }
-                } else {
+                } Else {
                     Write-Warning "$([char]34)$($sOutput)$([char]34) already exists."
                 }
-            } else {
+            } Else {
                 Write-Warning "$([char]34)$($WAVFile)$([char]34) not a wav file?"
             }
         }
-    } else {
+    } Else {
         Write-Warning "FFMPEG missing?!"
     }
 }
@@ -95,7 +96,7 @@ Function Convert-WavBulk {
         [string]$Root
     )
 
-    Write-Host "Bulk converting wave files to mp3."
+    Write-Host "Bulk converting wav files to mp3."
     Write-Host "Indexing files in $($Root)..."
 
     If (Test-Path $Root) {
@@ -103,13 +104,13 @@ Function Convert-WavBulk {
         
         If ($arrWavFiles.Length -gt 0) {
             Write-Host "Starting conversion of files..."
-            ForEach($WavFile in $arrWavFiles) {
-                Convert-ToMP3 -WAVFile $WavFile
+            ForEach($WavFile In $arrWavFiles) {
+                Convert-ToWav2MP3 -WAVFile $WavFile
             }
-        } else {
+        } Else {
             Write-Warning "No files found?"
         }
-    } else {
+    } Else {
         Write-Warning "$($Root) path not found?"
     }
 }
@@ -129,11 +130,11 @@ Function Main {
         [string[]]$Arguments
     )
 
-    [string]$MyExtractionFolder = "$($PSScriptRoot)\extracted_sfx"  # extraction folder
+    [string]$MyExtractionFolder = "$($Global:WorkingDir)\extracted_sfx"  # extraction folder
      
     # logic for cmdline arguments
     If ($Arguments) {
-        for($i = 0; $i -lt $Arguments.Length; $i++) {
+        For($i = 0; $i -lt $Arguments.Length; $i++) {
             #Write-Host "DEBUG : Arg $($i.ToString()) is $($Arguments[$i])"
 
             # default, a PWSH Switch statement on a String is always case insensitive
